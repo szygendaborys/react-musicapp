0a255720527ffa25c0750e65c08678667517fe51
Borys Szygenda <43183384+jhrwekuh@users.noreply.github.com>
2019-01-12 16:34:05 +0000

    Updates

---
+++ precache-manifest.8df4eea8feba923d1f2e77e534677008.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb065940398752971464b1044edc5926",
+    "url": "/react-musicapp/static/media/Drum3.bb065940.wav"
+  },
+  {
+    "revision": "2735ea035a1d849b5f05",
+    "url": "/react-musicapp/static/css/main.9152759c.chunk.css"
+  },
+  {
+    "revision": "78408ae11c2277325606c5c7c2e73a83",
+    "url": "/react-musicapp/static/media/Trumpet3.78408ae1.wav"
+  },
+  {
+    "revision": "2c4a8978e613f53f8c15",
+    "url": "/react-musicapp/static/js/1.2c4a8978.chunk.js"
+  },
+  {
+    "revision": "9f5ccb9568cd2bd8ea91",
+    "url": "/react-musicapp/static/js/runtime~main.9f5ccb95.js"
+  },
+  {
+    "revision": "b32361a3132629d660737622fedd91a9",
+    "url": "/react-musicapp/static/media/Drum1.b32361a3.wav"
+  },
+  {
+    "revision": "e3f315df95bbc96e8da1b100a7035ba8",
+    "url": "/react-musicapp/static/media/Drum2.e3f315df.wav"
+  },
+  {
+    "revision": "2735ea035a1d849b5f05",
+    "url": "/react-musicapp/static/js/main.2735ea03.chunk.js"
+  },
+  {
+    "revision": "e90e24b6051c4aa29fb01938bf241ea9",
+    "url": "/react-musicapp/static/media/Piano1.e90e24b6.mp3"
+  },
+  {
+    "revision": "eb819532abd8de665e2224bdc1456be3",
+    "url": "/react-musicapp/static/media/Piano2.eb819532.mp3"
+  },
+  {
+    "revision": "8c0c78ec1b371ddd6d1536a13e073854",
+    "url": "/react-musicapp/static/media/Piano3.8c0c78ec.mp3"
+  },
+  {
+    "revision": "cd90aa1e33ea5ebe87efa520b0d5ccf6",
+    "url": "/react-musicapp/static/media/Trumpet1.cd90aa1e.wav"
+  },
+  {
+    "revision": "032791a964f49bc5381d3669ad015d53",
+    "url": "/react-musicapp/static/media/Trumpet2.032791a9.wav"
+  },
+  {
+    "revision": "2c4a8978e613f53f8c15",
+    "url": "/react-musicapp/static/css/1.8040ca57.chunk.css"
+  },
+  {
+    "revision": "6bfb4f5fc1a01b0c7a84cad8d6248663",
+    "url": "/react-musicapp/index.html"
+  }
+];
